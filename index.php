e657459b23dc241a9f5c18544fa36c86de005455
Lucatro <lucatro02@gmail.com>
2018-09-07 14:56:32 +0200

    Added index.php and assets

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Eloh</title>
+	    <meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<meta name="keywords" content="Eloh design, Eloh webdevelopment, WebDevelopment service, Prestiges, Eloh mc-market, Eloh spigotmc, Eloh spigot, Eloh minecraft italia, Eloh arkem, Eloh lucatro, Eloh dragon3blu"/>
+		<meta name="description" content="Eloh official website"/>
+		<meta name="author" content="Lucatro/Eloh">
+		<meta name="theme-color" content="red">
+		<meta http-equiv="Cache-Control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="assets/js/main.js"></script>
+		<!--
+			Author: Lucatro
+			Date: 07/09/2018
+		-->
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-default">
+			<div class="container">
+			  <div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				  <span class="sr-only">Toggle navigation</span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Server Name</a>
+			  </div>
+			  <div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+				  <li><a href="#">1</a></li>
+				  <li><a href="#">2</a></li>
+				  <li><a href="#">3</a></li>
+				  <li><a href="#">4</a></li>
+				  <li><a href="#">5</a></li>
+				  <li><a href="#">6</a></li>
+				  <li><a href="#">7</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				  <li><a href="./"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+				  <li><a href="./"><i class="fas fa-lock"></i> Signup</a></li>
+				</ul>
+			  </div>
+			</div>
+		  </nav>
+		</header>
+		<section>
+			<div class="col-xs-12 text-center">
+				<img id="logo" src="https://auscraft.net/images/AusCraft-Logo-800px.png" />
+			</div>
+			<div class="container">
+				<div class="col-xs-12 col-md-8">
+					<h1 class="underline">About us</h1>
+				</div>
+				<div class="col-xs-12 col-md-4">
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
